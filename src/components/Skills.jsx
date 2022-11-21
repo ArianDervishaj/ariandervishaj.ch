2c811a08e60a1ed3c4d6e4a69fa7db6500f4fcf9
ArianDervishaj <ArianDervishaj@icloud.com>
2022-11-21 14:18:50 +0100

    s

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,7 +8,7 @@ function Skills(){
         
         <div className="w-full mt-20 border-2 border-black dark:border-white rounded-lg pl-5 pt-5 pb-5" >
             <div className="text-lg text-left dark:text-white">
-                <Title>Compétence Technique</Title>
+                <Title>Compétences Techniques</Title>
             </div>
             <div className="flex flex-row flex-wrap justify-center mt-8">
                 {skills.map(skill => (
