78026b776267cb59e45252d8bf5e50ce2bbc997b
ArianDervishaj <ArianDervishaj@icloud.com>
2023-01-23 02:07:07 +0100

    ajout padding

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,11 +4,11 @@ function Intro(){
     return(
         
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-            <h1 className="text-5xl md:text-7xl mb-4 md:mb-6 font-bold bg-gradient-to-br from-fuchsia-500 to-cyan-500 bg-clip-text text-transparent">
+            <h1 className="text-5xl md:text-7xl p-2 mb-4 md:mb-6 font-bold bg-gradient-to-br from-fuchsia-700 to-cyan-700 dark:from-fuchsia-500 dark:to-cyan-500 bg-clip-text text-transparent">
 
                 Arian Dervishaj
             </h1>
-            <p className="text-base md:text-xl mb-3 font-medium">Etudiant en informatique</p>
+            <p className="text-base md:text-xl mb-3 mt-2 font-medium">Etudiant en informatique</p>
             <p className="text-sm max-w-xl mb-6 font-bold">
             Étudiant en informatique, je suis passionné d'informatique depuis mon plus jeune âge.
              Lors de ma formation, j'ai eu la chance de pouvoir m'initier à plusieurs langages de programmation
