4285b99cd31348088acdc77a7691875e4d4a2b26
ArianDervishaj <ArianDervishaj@icloud.com>
2023-01-23 02:07:14 +0100

    changement couleur bouton

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,7 +32,7 @@ function Contact(){
                     <button
                         type="submit"
                         className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white 
-                        bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white">
+                        bg-gradient-to-tl from-fuchsia-700 to-cyan-700 dark:from-fuchsia-500 dark:to-cyan-500  drop-shadow-md hover:stroke-white">
                         Envoyer
                     </button>
 
