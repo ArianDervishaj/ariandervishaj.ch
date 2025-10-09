78386cc9843a38deec425feddcdbdcada52b1e9d
Arian <ariandervishaj@icloud.com>
2025-10-10 01:34:09 +0200

    better site map, robots txt, remove rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,31 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
-import sitemap from "@astrojs/sitemap"
+import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://neonmint.efeele.dev",
-  integrations: [preact(), icon(), sitemap({
-    filter: (page) =>
-      !page.includes("/blog/tags") &&
-      !page.includes("/blog/techs"),
-  }),],
+  site: "https://ariandervishaj.ch",
+  integrations: [
+    preact(),
+    icon(),
+    sitemap({
+      serialize(item) {
+        // Just set priority and lastmod
+        if (item.url === 'https://ariandervishaj.ch/') {
+          item.priority = 1.0;
+        } else if (item.url.includes('/portfolio/projects/')) {
+          item.priority = 0.8;
+        }
+
+        // Convert Date to ISO string
+        item.lastmod = new Date().toISOString();
+
+        return item;
+      }
+    })
+  ],
 
   vite: {
     plugins: [tailwindcss()],
